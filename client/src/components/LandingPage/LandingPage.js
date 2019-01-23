03d07c73524358658ebec55bf72dd2dcc606e40b
ericremoroza <eric.remoroza@outlook.com>
2019-01-23 11:20:20 -0500

    do not merge with master

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,16 +1,16 @@
 import React from "react";
-import {Container} from "../Grid";
+import { Container } from "../Grid";
 import "../../styles.css"
 import Google from "../Signin";
 
 const LandingPage = () => (
     <Container>
-    <div className="landing">
-    <h1>Perfect Partner</h1>
-    <h6>Outsource Your Thoughtfulness</h6>
-    <div className="googleBox"><Google/></div>
-    </div>
-    
+        <div className="landing">
+            <h1>Perfect Partner</h1>
+            <h6>Outsource Your Thoughtfulness</h6>
+            <div className="googleBox"><Google /></div>
+        </div>
+
     </Container>
 )
 
